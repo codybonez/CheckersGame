e014b587fb68fb30c6030a127b0524357896eee9
Cody Norwood <CodyBonez@yahoo.com>
2026-05-12 15:11:22 -0700

    Add project files.

---
+++ CheckersGame/Board.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CheckersGame
+{
+    public class Board
+    {
+        bool isPieceColorOnBlackSquare;
+        int[,] CheckerBoard = new int[8, 8];
+     
+        
+
+        public Board(int[,] checkerboard)
+        {
+            this.CheckerBoard = checkerboard;
+
+
+        }
+
+        public int[,] InitializeBoard(int[,] board, List<Pieces> pieces)
+        {
+           
+            return board;
+        }
+    }
+}
---
+++ CheckersGame/Pieces.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersGame
+{
+    public class Pieces
+    {
+        // add piece status next
+        public PieceColor Color;
+        public Pieces() { 
+        
+        }
+
+    }
+}
